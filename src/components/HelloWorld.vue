5341a0f4492c804f4bd63303bd9beb738ddfab56
liuyonghua-cww <53337014+liuyonghua-cww@users.noreply.github.com>
2023-06-01 10:23:27 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <span @click="increment">{{ state.number }}</span>
+</template>
+<script setup lang="ts">
+import { ref, reactive, nextTick } from 'vue';
+
+defineProps<{ msg: string }>();
+
+const state = reactive({ number: 1 });
+const increment = () => {
+  state.number++
+};
+nextTick(() => {
+  console.log('nextTick')
+})
+</script>
+
+<style scoped></style>
